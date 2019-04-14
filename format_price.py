7385b4e3f98456862ef5dabde4c3499e6109f9df
cr00z <cruz@list.ru>
2019-04-14 09:01:36 +0700

    write function format_price

--- format_price.py
+++ format_price.py
@@ -1,5 +1,13 @@
 def format_price(price):
-    pass
+    try:
+        float_price = round(float(price), 2)
+        if float_price - int(float_price) != 0:
+            return '{:,.2f}'.format(float_price).replace(',', ' ')
+        else:
+            return '{:,}'.format(int(float_price)).replace(',', ' ')
+    except (ValueError, TypeError):
+        return None
+
 
 if __name__ == '__main__':
-    pass
+    print(format_price(None))
