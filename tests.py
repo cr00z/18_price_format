d94adca59b865733f66bd25387167bafecc053aa
cr00z <netrebinr@gmail.com>
2019-04-14 13:53:42 +0700

    fix pep8 violation warnings

--- tests.py
+++ tests.py
@@ -4,42 +4,35 @@ from format_price import format_price
 
 
 class FormatPriceTestCase(unittest.TestCase):
-    def test_returns_price_for_intstr(self):
+    def test_intstr_to_price(self):
         pretty_price = format_price('123456789')
         self.assertEqual(pretty_price, '123 456 789')
 
-
-    def test_returns_price_for_floatstr(self):
+    def test_floatstr_to_price(self):
         pretty_price = format_price('12345.6789')
         self.assertEqual(pretty_price, '12 345.68')
 
-
-    def test_returns_price_for_neg_intstr(self):
+    def test_neg_intstr_to_price(self):
         pretty_price = format_price('-123456789')
         self.assertEqual(pretty_price, '-123 456 789')
 
-
-    def test_returns_price_for_neg_floatstr(self):
+    def test_neg_floatstr_to_price_(self):
         pretty_price = format_price('-12345.6789')
         self.assertEqual(pretty_price, '-12 345.68')
 
-
-    def test_returns_none_for_nondigit(self):
+    def test_nondigit_to_none(self):
         pretty_price = format_price('1.1a')
         self.assertIsNone(pretty_price)
 
-
-    def test_returns_none_for_none(self):
+    def test_none_to_none(self):
         pretty_price = format_price(None)
         self.assertIsNone(pretty_price)
 
-
-    def test_returns_none_for_object(self):
+    def test_object_to_none(self):
         pretty_price = format_price([1])
         self.assertIsNone(pretty_price)
 
-
-    def test_returns_price_for_exponential(self):
+    def test_exponential_to_price_(self):
         pretty_price = format_price('123456789e-3')
         self.assertEqual(pretty_price, '123 456.79')
 
